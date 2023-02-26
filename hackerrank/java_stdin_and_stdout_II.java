e7954d1ef1f1956adb4a4c6be9c925e9a591c174
Prabhneet Narula <imailcode@gmail.com>
2023-02-27 00:26:09 +0530

    java_stdin_and_stdout_II added.

---
+++ hackerrank/java_stdin_and_stdout_II.java
@@ -0,0 +1,19 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class java_stdin_and_stdout_II {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+        scan.close();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
