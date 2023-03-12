489d5f1fea7f2298abbf8038294bfcac64b29787
Prabhneet Narula <imailcode@gmail.com>
2023-03-12 14:14:22 +0530

    circumference added.

---
+++ general_practice/circumference.java
@@ -0,0 +1,19 @@
+package general_practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class circumference {
+
+    public static double circumference(int radius) {
+        return 2 * 3.14 * radius;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        int radius = Integer.parseInt(buffer.readLine());
+
+        System.out.println("Circumference of the circle: " + circumference(radius));
+    }
+}
